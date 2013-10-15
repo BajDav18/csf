a68a6195a2106e86807f62418ada825a4fe85527
BajDav18 <davidbajuk@gmail.com>
2013-10-15 14:09:43 -0700

    hw2 finished

---
+++ hw2.py
@@ -0,0 +1,100 @@
+# Name: David Bajuk
+# Evergreen Login: BajDav18
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 2
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+import math
+import hw2_test
+
+###
+### Problem 1
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 1 solution follows:"
+
+i = 1
+x = 0
+while (i < (hw2_test.n + 1)):
+    i + x
+    x = i + x
+    i = i + 1
+print x
+
+
+###
+### Problem 2
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 2 solution follows:"
+
+for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    x = 1.0/i
+    print x
+
+
+###
+### Problem 3
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 3 solution follows:"
+
+n = 10
+triangular = 0
+for i in [1,2,3,4,5,6,7,8,9,n]:
+    triangular = triangular + i
+print "Triangular number", n, "via loop:", triangular
+print "Triangular number", n, "via formula:", n*(n+1)/2 
+
+###
+### Problem 4
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 4 solution follows:"
+
+n = 10
+factorial = 1
+for i in [1,2,3,4,5,6,7,8,9,n]:
+    factorial = factorial * i
+print factorial 
+
+###
+### Problem 5
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 5 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Problem 6
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 6 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Collaboration
+###
+#Jacob Morris MorJac05
+# ... List your collaborators and other sources of help here (websites, books, etc.),
+# ... as a comment (on a line starting with "#").
+
+###
+### Reflection
+###
+
+# Way too long. I do not think i recieved enough information to be able
+# to complete every problem, specifically problem 5 and 6.
